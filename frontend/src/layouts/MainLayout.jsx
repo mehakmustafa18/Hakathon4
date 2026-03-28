8d79519e2cc961f1c555146dab8895c8e13c4acd
mehakmustafa18 <mehakch160@gmail.com>
2026-03-28 19:54:12 +0500

    change

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -1,10 +1,10 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from '../components/Navbar';
-import Footer from '../components/Footer';
+import { Outlet } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Footer from "../components/Footer";
 
 const MainLayout = () => {
   return (
-    <div className="flex flex-col min-h-screen bg-brand-dark selection:bg-brand-red selection:text-white">
+    <div className="wraperflex flex-col min-h-screen bg-brand-dark selection:bg-brand-red selection:text-white">
       <Navbar />
       <main className="flex-grow">
         <Outlet />
